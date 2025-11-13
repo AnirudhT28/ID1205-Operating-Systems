b8d3f10fb170a35dce2a2e9a9de767a504fd4c98
AnirudhT28 <anirudhtiwari2805@gmail.com>
2025-11-13 23:20:49 +0100

    allocate memory for array and print random numbers

--- question_8.c
+++ question_8.c
@@ -4,15 +4,40 @@
 #include <time.h>       
 #include <sys/wait.h>   
 #include <sys/types.h>
+#include <math.h>
 
 int main(int argcount, char *arglist[]) {
 
-if (argcount != 2) { // if there is no function argument
-        fprintf(stderr, "include function argument", arglist[0]);
-        exit(1); // Exit with an error
+   
+    if (argcount != 2) { // check if there was any more or less than 1 argument passed
+      
+       fprintf(stderr, "Usage: %s <N>\n", arglist[0]);   // give out an error message
+        exit(1); 
     }
     
-int N = atoi(arglist[1]); // take the value from arglist[1] from the argument and convert it into an integer 
+    int N = atoi(arglist[1]); 
+    
+    double *array = (double *)malloc(N * sizeof(double)); // allocate memory for array of size N
+    
+
+    srand((unsigned int)time(NULL)); // seed the random function
+
+     for (int i = 0; i < N; i++) {
+        array[i] = (double)rand() / (double)RAND_MAX; // RAND_MAX is always > rand, cannot be 0 aswell
+     }
 
 
+
+
+
+
+
+
+    // 3. This is the corrected print statement
+ for (int i = 0; i < N; i++) {
+        printf("%f ", array[i]);
+    }
+
+    free(array);
+    return 0;
 }
