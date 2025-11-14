57043065b208d3172388e41b70f656e11e6c6755
AnirudhT28 <anirudhtiwari2805@gmail.com>
2025-11-14 18:27:34 +0100

    fixed make file

--- assignment1/Makefile.c
+++ assignment1/Makefile.c
@@ -1,26 +1,30 @@
+
 CC = gcc
-	
-LDFLAGS = -lm -lrt
-	
+
+
 CFLAGS = -Wall -Wextra -std=c11 -g
 
+ //Define the name of your source file(s)
 SRCS = question_8.c
 
-
+// Define the name of your final executable
 TARGET = question_8
 
 
-.PHONY: all clean
+LDFLAGS = -lm -lrt 
 
+
+
+.PHONY: all clean
 
 all: $(TARGET)
 
 
 $(TARGET): $(SRCS)
-
+	
 	$(CC) $(CFLAGS) -o $(TARGET) $(SRCS) $(LDFLAGS)
 
+
 clean:
-	# The '-' in front of 'rm' means 'make' will not
-	# stop if it fails (e.g., if the file isn't there).
+
 	-rm -f $(TARGET)
