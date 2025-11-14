436ff90632ae8b9a2f46aa458ec64fa2ca8ed28a
AnirudhT28 <anirudhtiwari2805@gmail.com>
2025-11-14 18:38:51 +0100

    IT WORKS YIPPEEE

--- assignment1/question_8.c
+++ assignment1/question_8.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>      
 #include <stdlib.h>     
 #include <unistd.h>          
@@ -5,7 +6,6 @@
 #include <sys/types.h>
 #include <math.h>
 #include <string.h>
-#define _POSIX_C_SOURCE 199309L
 #include <time.h>
 #define READ_END 0
 #define WRITE_END 1
