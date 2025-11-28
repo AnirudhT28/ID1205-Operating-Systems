55e99b3aa18dfce2bd4d4145b33805264a76921a
AnirudhT28 <anirudhtiwari2805@gmail.com>
2025-11-28 19:05:20 +0100

    refactorer source code

--- assignment_2/question6/question_6.c
+++ assignment_2/question6/question_6.c
@@ -7,13 +7,9 @@
 
 #define ARRAY_SIZE 1000000
 
-/* Global variables to be accessible by threads */
 int num_threads = 0;
-float *data_array; // The array of data
-
+float *data_array; 
 void *thread_func(void *arg); /* the thread function */
-
-/* Helper for timing */
 double get_time_diff(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 }
@@ -26,61 +22,51 @@ int main(int argc, char *argv[])
     }
 
     num_threads = atoi(argv[1]);
-    if (num_threads <= 0) {
-        printf("Error: num_threads must be > 0\n");
-        return 1;
-    }
 
     /* Initialize an array of random values */
     data_array = (float *)malloc(ARRAY_SIZE * sizeof(float));
-    if (data_array == NULL) {
-        perror("Failed to allocate array");
-        return 1;
-    }
+  
 
     srand(time(NULL));
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        data_array[i] = (float)rand() / (float)RAND_MAX; // Random float in [0,1]
+        data_array[i] = (float)rand() / (float)RAND_MAX; // Random float in [0,1] resuing code from last assignment
     }
 
-    /* Perform Serial Sum */
+   
     double sum_serial = 0.0;
     double time_serial = 0.0;
     struct timespec start_serial, end_serial;
 
-    // Timer Begin
-    clock_gettime(CLOCK_MONOTONIC, &start_serial);
+    clock_gettime(CLOCK_MONOTONIC, &start_serial); // start timer
     
     for (int i = 0; i < ARRAY_SIZE; i++) {
         sum_serial += data_array[i];
     }
     
-    clock_gettime(CLOCK_MONOTONIC, &end_serial);
-    // Timer End
+    clock_gettime(CLOCK_MONOTONIC, &end_serial); //end serial timer
+  
 
-    time_serial = get_time_diff(start_serial, end_serial);
+time_serial = (end_serial.tv_sec - start_serial.tv_sec) + 
+              (end_serial.tv_nsec - start_serial.tv_nsec) / 1000000000.0;
     printf("Serial Sum = %.2f, time = %.5f \n", sum_serial, time_serial);
 
     /* Create a pool of num_threads workers and keep them in workers */
     pthread_t *workers = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
-    int *thread_ids = (int *)malloc(num_threads * sizeof(int)); // Memory to pass IDs safely
+    int *thread_ids = (int *)malloc(num_threads * sizeof(int)); 
     
     double time_parallel = 0.0;
     double sum_parallel = 0.0;
     struct timespec start_parallel, end_parallel;
-
-    // Timer Begin
-    clock_gettime(CLOCK_MONOTONIC, &start_parallel);
+    clock_gettime(CLOCK_MONOTONIC, &start_parallel); // start parallel timer
 
     for (int i = 0; i < num_threads; i++) {
         pthread_attr_t attr;
         pthread_attr_init(&attr);
-        
-        // Assign ID
+    
         thread_ids[i] = i; 
         
-        // Pass the address of the ID to the thread
-        if (pthread_create(&workers[i], &attr, thread_func, &thread_ids[i]) != 0) {
+       
+        if (pthread_create(&workers[i], &attr, thread_func, &thread_ids[i]) != 0) {  // Pass the address of the ID to the thread
             perror("Failed to create thread");
         }
     }
@@ -89,17 +75,20 @@ int main(int argc, char *argv[])
         void *retval;
         pthread_join(workers[i], &retval);
         
-        // Aggregate results
         if (retval != NULL) {
-            sum_parallel += *(double*)retval;
-            free(retval); // Free the memory allocated inside thread_func
+            sum_parallel += *(double*)retval; // sum results
+            free(retval); // Free the memory inside threads
         }
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &end_parallel);
-    // Timer End
+    clock_gettime(CLOCK_MONOTONIC, &end_parallel); // end parallel timer
 
-    time_parallel = get_time_diff(start_parallel, end_parallel);
+
+
+time_parallel = (end_parallel.tv_sec - start_parallel.tv_sec) + 
+              (end_parallel.tv_nsec - start_parallel.tv_nsec) / 1000000000.0;
+
+
     printf("Parallel Sum = %.2f, time = %.5f \n", sum_parallel, time_parallel);
 
     /* free up resources properly */
@@ -125,16 +114,11 @@ void *thread_func(void *arg) {
     }
 
     /* Perform Partial Parallel Sum Here */
-    // allocating on heap to return safely
     double *my_sum_ptr = (double *)malloc(sizeof(double)); 
     *my_sum_ptr = 0.0;
 
     for (int i = start_index; i < end_index; i++) {
         *my_sum_ptr += data_array[i];
     }
-
-    // Optional: Print debug info (comment out for cleaner output on large thread counts)
-    // printf("Thread %d sum = %f (Range: %d - %d)\n", my_id, *my_sum_ptr, start_index, end_index);
-    
     pthread_exit((void*)my_sum_ptr);
 }
